490a8bc3fcc3ef705aa110b364a11b91e0fba488
Dung Ngo <dungnc@paxcreation.com>
2019-12-13 14:57:39 +0700

    Update gallery

--- app/Http/Controllers/Admin/ImageController.php
+++ app/Http/Controllers/Admin/ImageController.php
@@ -8,23 +8,27 @@ use App\Helpers\CommonHelper;
 
 class ImageController extends Controller
 {
+    function get_file_extension($file_name) {
+        return substr(strrchr($file_name,'.'),1);
+    }
+
     public function uploadImageBase64(Request $request)
     {
         $image = $request->image;
         if($image){
-            $fileName = $request->file_name;
+            $fileExt = $this->get_file_extension($request->file_name);
             list($type, $image) = explode(';', $image);
             list(, $image)      = explode(',', $image);
 
             $image = base64_decode($image);
-            $imagePath= 'tmp/' . uniqid() . '_' . $fileName;
-            Storage::disk('public')->put($imagePath, $image);
+            $imagePath= 'tmp/' . uniqid() . '.' . $fileExt;
+            Storage::put($imagePath, $image);
             //CommonHelper::compress(public_path('storage/').$imagePath);
             
             return response()->json([
                 'status' => 200,
                 'pathTmpFile' => $imagePath,
-                'url' => asset(Storage::disk('public')->url($imagePath))
+                'url' => asset(Storage::url($imagePath))
             ]);
         }
         return response()->json(['status' => 500]);
