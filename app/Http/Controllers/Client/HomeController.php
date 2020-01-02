d4ccb1a14229ba265366506e48d4d5a26fefdb15
Dung Ngo <dungnc@paxcreation.com>
2020-01-02 21:54:31 +0700

    Fix bug

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -350,7 +350,7 @@ class HomeController extends Controller
         return view('client.detail-course', [
             'title' => $course->title,
             'course' => $course,
-            'listArticle' => $course->articles()->orderBy('priority', 'desc')->get(),
+            'listArticle' => $course->articles()->orderBy('priority', 'desc')->orderBy('id', 'desc')->get(),
             'comments' => $comments,
             'videoLearned' => $videoLearned
         ]);
